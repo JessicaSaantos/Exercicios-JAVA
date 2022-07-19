2d0ecfeed8382cb8a7151977edd0b34026ca8bb1
JessicaSaantos <jessicaferre998@gmail.com>
2022-07-19 15:04:48 -0300

    adicionando arquivos

---
+++ src/one/digitalinnovation/Main.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Calculadora
+		System.out.println("Exercício calculadora");
+		Calculadora.soma(25, 24);
+		Calculadora.subtracao(14, 2.8);
+		Calculadora.multiplicacao(7, 4);
+		Calculadora.divisao(12, 2.5);
+		
+		// Mensagem
+		System.out.println("Exercício mensagem");
+		Mensagem.obterMensagem(9);
+		Mensagem.obterMensagem(15);
+		Mensagem.obterMensagem(23);
+		// Empréstimo
+		System.out.println("Exercício empréstimo");
+		Emprestimo.calcular(1000 , Emprestimo.getDuasParcelas());
+		Emprestimo.calcular(1000 , Emprestimo.getTresParcelas());
+		Emprestimo.calcular(1000 , 4);
+	}
+
+}
